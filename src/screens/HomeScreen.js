d19575a79b44fab9a3a94cec1bcbe9848e752b3d
David Pearl <davidtpearl@gmail.com>
2021-08-02 21:26:19 +1000

    Navigation between screens

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,35 @@
-import React from "react"
-import { Text, StyleSheet } from "react-native"
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Away Screen</Text>;
+const HomeScreen = (props) => {
+  console.log(props);
+  const { navigation } = props;
+  return (
+    <View>
+      <Text style={styles.text}>Home Screen</Text>
+      <Button
+        title="Go to Components Screen"
+        onPress={() => {
+          console.log("Pressed go to components screen");
+          navigation.navigate("Components");
+        }}
+      />
+      <TouchableOpacity
+        onPress={() => {
+          console.log("Pressed Go to list button");
+          navigation.navigate("List");
+        }}
+      >
+        <Text>Go to List Screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
