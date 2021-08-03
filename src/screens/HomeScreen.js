d05a63c9d7d62a1baa398d74efda6075806e2351
David Pearl <davidtpearl@gmail.com>
2021-08-03 22:18:15 +1000

    Test styling of touchable opacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = (props) => {
   console.log(props);
   const { navigation } = props;
   return (
     <View>
-      <Text style={styles.text}>Home Screen</Text>
+      <Text style={styles.headerText}>Home Screen</Text>
       <Button
         title="Go to Components Screen"
         onPress={() => {
           console.log("Pressed go to components screen");
           navigation.navigate("Components");
         }}
+        style={styles.buttonStyle}
       />
       <Button
         title="Go to Components Images"
@@ -20,6 +21,7 @@ const HomeScreen = (props) => {
           console.log("Pressed go to images screen");
           navigation.navigate("Images");
         }}
+        style={styles.buttonStyle}
       />
       <Button
         title="Go to List Screen"
@@ -27,22 +29,35 @@ const HomeScreen = (props) => {
           console.log("Pressed go to List screen");
           navigation.navigate("List");
         }}
+        style={styles.buttonStyle}
       />
-      {/* <TouchableOpacity
+      <TouchableOpacity
         onPress={() => {
-          console.log("Pressed Go to list button");
-          navigation.navigate("List");
+          console.log("Pressed Go to images button");
+          navigation.navigate("Images");
         }}
+        style={styles.buttonStyle}
       >
-        <Text>Go to List Screen</Text>
-      </TouchableOpacity> */}
+        <Text style={styles.text}>Go to Images Screen</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
+  headerText: {
     fontSize: 30,
+    color: "#000",
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+  },
+  buttonStyle: {
+    margin: 20,
+    padding: 20,
+    color: "white",
+    backgroundColor: "blue",
   },
 });
 
